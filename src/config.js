ce1118921e4b1272900755dce5c3431faea5a1d4
stupid cat <cat@blargbot.xyz>
2020-02-10 13:37:06 -0700

    eughhhh

--- src/config.js
+++ src/config.js
@@ -8,13 +8,15 @@ let config;
 try {
   config = require('../config'); // load either js or json files
 } catch (err) {
-  console.error('Failed to load the config file. Using default...');
+  console.error('Error loading config file. Please ensure it exists and try again.');
+  process.exit(1);
+  // console.error('Failed to load the config file. Using default...');
 
-  // copy over the default config file
-  fs.copyFileSync(path.join(__dirname, '..', 'default-config.js'), path.join(__dirname, '..', 'config.js'));
+  // // copy over the default config file
+  // fs.copyFileSync(path.join(__dirname, '..', 'default-config.js'), path.join(__dirname, '..', 'config.js'));
 
-  // load the default config file
-  config = require('../default-config.js');
+  // // load the default config file
+  // config = require('../default-config.js');
 }
 
 module.exports = config;
